60ee452abba3d1b7f2fc660a547efae2f14cd398
Deepak Warrier <bunch.of.food@gmail.com>
2018-10-19 23:30:34 -0400

    Initial Commit of the Project Files

---
+++ app/src/main/java/com/example/deepakwarrier/growth/WelcomeActivity.java
@@ -0,0 +1,16 @@
+package com.example.deepakwarrier.growth;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * A login screen that offers login via email/password.
+ */
+public class WelcomeActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
+
