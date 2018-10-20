b73012710a7c74702a22c6f6c5df618846379d71
marcus59876 <marcus.mckinley13@gmail.com>
2018-10-20 07:27:06 -0400

    Fixed problem with images not loading

--- app/src/main/java/com/example/deepakwarrier/growth/RelaxationDetails.java
+++ app/src/main/java/com/example/deepakwarrier/growth/RelaxationDetails.java
@@ -28,47 +28,39 @@ public class RelaxationDetails extends AppCompatActivity {
 
         kb = new GrowthDatabase(new File(getFilesDir(), "GrowthKB.save"));
 
-
-       /* FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        }); */
+        
 
         TextView txt = (TextView) findViewById(R.id.text1);
         txt.setText(mostUsed);
-        if (mostUsed == "Reading improves your ability to comprehend written material—a skill helpful in any career.") {
+        if (mostUsed.equals("Reading improves your ability to comprehend written material—a skill helpful in any career.")) {
             imageView.setImageResource(R.mipmap.reading);
         }
 
-        if (mostUsed == "If you are going through a tough time, talking to someone might sound simplistic but it really is one of the best possible things you can do.") {
+        if (mostUsed.equals("If you are going through a tough time, talking to someone might sound simplistic but it really is one of the best possible things you can do.")) {
             imageView.setImageResource(R.mipmap.phone);
         }
 
-        if (mostUsed == "Taking a 30-minute walk a day is kind of like that proverbial apple: There's a good chance it'll keep the doctor away") {
+        if (mostUsed.equals("Taking a 30-minute walk a day is kind of like that proverbial apple: There's a good chance it'll keep the doctor away")) {
             imageView.setImageResource(R.mipmap.walking);
         }
 
-        if (mostUsed == "Yoga is known for its ability to ease stress and promote relaxation.") {
+        if (mostUsed.equals("Yoga is known for its ability to ease stress and promote relaxation.")) {
             imageView.setImageResource(R.mipmap.yoga);
         }
 
-        if (mostUsed == "Breathing exercises are a good way to relax, reduce tension, and relieve stress.") {
+        if (mostUsed.equals("Breathing exercises are a good way to relax, reduce tension, and relieve stress.")) {
             imageView.setImageResource(R.mipmap.breathing);
         }
 
-        if (mostUsed == "Participating in a regular stretching program not only helps increase your flexibility, but it can also calm your mind") {
+        if (mostUsed.equals("Participating in a regular stretching program not only helps increase your flexibility, but it can also calm your mind")) {
             imageView.setImageResource(R.mipmap.stretch);
         }
 
-        if (mostUsed == "“Keeping a personal journal a daily in-depth analysis and evaluation of your experiences is a high-leverage activity that increases self-awareness and enhances all the endowments and the synergy among them.” — Stephen R.Covey") {
+        if (mostUsed.equals("“Keeping a personal journal a daily in-depth analysis and evaluation of your experiences is a high-leverage activity that increases self-awareness and enhances all the endowments and the synergy among them.” — Stephen R.Covey")) {
             imageView.setImageResource(R.mipmap.writing);
         }
 
-        if (mostUsed == "You can improve the quality of other people's lives, and in the process of doing so you can benefit yourself as well.") {
+        if (mostUsed.equals("You can improve the quality of other people's lives, and in the process of doing so you can benefit yourself as well.")) {
             imageView.setImageResource(R.mipmap.yoga);
         }
     }
