be72ef9f38935dd7962098cca96c96d0f8d70452
Rohit Gangupantulu <rbg5181@gmail.com>
2018-10-20 03:57:37 -0400

    Added crisis button + dialing functionality

--- app/src/main/java/com/example/deepakwarrier/growth/MainActivity.java
+++ app/src/main/java/com/example/deepakwarrier/growth/MainActivity.java
@@ -1,24 +1,50 @@
 package com.example.deepakwarrier.growth;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+
 // import android.support.v7.app.ActionBarActivity;
 // import android.view.Menu;
 public class MainActivity extends AppCompatActivity {
 
     // public static GrowthDatabase kb = new GrowthDatabase("GrowthKB.save");
-   //  ActionBar actionbar = getActionBar();
-   //  actionBar.hide();
+    //  ActionBar actionbar = getActionBar();
+    //  actionBar.hide();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.crisis).setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+               dialPhone();
+            }
+        });
     }
 
+    public void dialPhone() {
+        Intent callIntent = new Intent(Intent.ACTION_DIAL);
+        callIntent.setData(Uri.parse("tel:6103333244"));
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.CALL_PHONE}, 0);
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        startActivity(callIntent);
+    }
     public void startContacts(View view) {
         startActivity(new Intent(this, MyContacts.class));
     }
