f838d2a7fc2f191694c63d0e94ec455416f488b9
epowerj <epowerj@gmail.com>
2018-10-19 20:50:24 -0400

    Added GrowthDatabase

---
+++ app/src/main/java/com/example/deepakwarrier/growth/GrowthDatabase.java
@@ -0,0 +1,19 @@
+package com.example.deepakwarrier.growth;
+
+public class GrowthDatabase {
+
+    String saveFilePath = "testfile.sav";
+
+
+    public void load(String filepath){
+
+    }
+
+    public void save(String filepath){
+
+    }
+
+    public String getSaveFilePath(){
+        return saveFilePath;
+    }
+}
