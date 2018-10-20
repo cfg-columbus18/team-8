3e2583a5855f5ed4ac6eb183704796d4afe93add
Kiana Harris <kianaharris4@gmail.com>
2018-10-20 05:26:52 -0400

    updated fonts and sizes for dashboard

--- app/src/main/java/com/example/deepakwarrier/growth/notification_center.java
+++ app/src/main/java/com/example/deepakwarrier/growth/notification_center.java
@@ -1,7 +1,9 @@
 package com.example.deepakwarrier.growth;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class notification_center extends AppCompatActivity {
 
@@ -9,5 +11,23 @@ public class notification_center extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notification_center);
+    }
+
+    public void startContacts(View view) {
+        startActivity(new Intent(this, ContactsActivity.class));
+    }
+
+    public void startActivities(View view) {
+       startActivity(new Intent(this, Relaxation.class));
+    }
+
+    public void startNotifications(View view) {
+//        startActivity(new Intent(this, Relaxation.class));
+    }
+
+    public void startGoals(View view) {
+    }
+
+    public void startGrowth(View view) {
     }
 }
