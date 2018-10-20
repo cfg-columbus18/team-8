49c6a291cece2454262d026046261d1c45536dc4
Deepak Warrier <bunch.of.food@gmail.com>
2018-10-19 21:49:02 -0400

    Initial Commit of the Project Files

--- app/src/main/java/com/example/deepakwarrier/growth/MainActivity.java
+++ app/src/main/java/com/example/deepakwarrier/growth/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.deepakwarrier.growth;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -18,18 +19,18 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void startContacts(View view) {
-        startActivity(new Intent(this, SurveyContacts.class));
+//        startActivity(new Intent(this, SurveyContacts.class));
     }
 
     public void startActivities(View view) {
-        startActivity(new Intent(this, ActivitiesRelax.class));
+//        startActivity(new Intent(this, ActivitiesRelax.class));
     }
 
     public void startNotifications(View view) {
-        startActivity(new Intent(this, NotificationSettings.class));
+//        startActivity(new Intent(this, NotificationSettings.class));
     }
 
     public void callCrisis(View view) {
-        startActivity(new Intent(Intent.ACTION_DIAL, "tel:6147178257"));//4402608595
+        startActivity(new Intent(Intent.ACTION_DIAL, Uri.parse("tel:6147178257")));//4402608595
     }
 }
