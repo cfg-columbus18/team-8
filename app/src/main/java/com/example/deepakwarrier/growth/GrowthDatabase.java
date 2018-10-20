d5f383cb3af4fc72517b6cc9976c38bc99a4f7f9
epowerj <epowerj@gmail.com>
2018-10-20 00:58:02 -0400

    Added debug to GrowthDatabase

--- app/src/main/java/com/example/deepakwarrier/growth/GrowthDatabase.java
+++ app/src/main/java/com/example/deepakwarrier/growth/GrowthDatabase.java
@@ -1,5 +1,7 @@
 package com.example.deepakwarrier.growth;
 
+import android.util.Log;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -28,6 +30,7 @@ public class GrowthDatabase {
 
     // load from save file
     public void load(){
+        Log.d("DB", "Loading...");
 
         // if there already is a save file, load
         if(saveFile.exists() && !saveFile.isDirectory()) {
@@ -45,23 +48,29 @@ public class GrowthDatabase {
                     line = br.readLine();
                 }
 
+                Log.d("DB", "Loaded");
+
                 allText = sb.toString();
 
             } catch (FileNotFoundException e) {
+                Log.d("DB", "File not found!");
                 e.printStackTrace();
             } catch (IOException e) {
+                Log.d("DB", "IOException!");
                 e.printStackTrace();
             }
 
             // allText should be updated now
 
             try {
+                Log.d("DB", "Building JSON...");
                 // build a JSON object
                 jsonObject = new JSONObject(allText);
                 if (!jsonObject.getString("status").equals("OK"))
                     return;
 
             } catch (JSONException e) {
+                Log.d("DB", "JSONException");
                 e.printStackTrace();
             }
 
@@ -73,23 +82,27 @@ public class GrowthDatabase {
 
     // save to file
     public void save(){
-        //TODO save
+        Log.d("DB", "Saving...");
 
         String toSave = jsonObject.toString();
 
         try(BufferedWriter bw = new BufferedWriter(new FileWriter(saveFile))) {
 
+            Log.d("DB", "Writing...");
             bw.write(toSave);
 
         } catch (FileNotFoundException e) {
+            Log.d("DB", "File not Found!");
             e.printStackTrace();
         } catch (IOException e) {
+            Log.d("DB", "IOException!");
             e.printStackTrace();
         }
     }
 
     // create a default starter state
     private void createDefaultStartState(){
+        Log.d("DB", "Creating Default Start state");
 
         try {
             jsonObject = new JSONObject("\n" +
@@ -97,6 +110,7 @@ public class GrowthDatabase {
                     "    \"activityPoints\": 10\n" +
                     "}");
         } catch (JSONException e) {
+            Log.d("DB", "JSONException");
             e.printStackTrace();
         }
 
@@ -110,10 +124,13 @@ public class GrowthDatabase {
 
     // returns current state of forest growth
     public int getCurrentForestGrowth(){
-        int result = 0;
+        Log.d("DB", "Getting forest growth");
+
+        int result = -1;
         try {
             result = jsonObject.getInt("activityPoints") / 2;
         } catch (JSONException e) {
+            Log.d("DB", "JSONException");
             e.printStackTrace();
         }
 
