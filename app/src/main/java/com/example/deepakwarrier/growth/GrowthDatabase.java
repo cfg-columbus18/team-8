2dc5c8d28fb4663b000f7ce6c0fc4fe47da1b4df
epowerj <epowerj@gmail.com>
2018-10-19 21:04:14 -0400

    Added getCurrentForestGrowth() to GrowthDatabase

--- app/src/main/java/com/example/deepakwarrier/growth/GrowthDatabase.java
+++ app/src/main/java/com/example/deepakwarrier/growth/GrowthDatabase.java
@@ -2,18 +2,37 @@ package com.example.deepakwarrier.growth;
 
 public class GrowthDatabase {
 
-    String saveFilePath = "testfile.sav";
+    private String saveFilePath = "testfile.sav";
+
+    private int activityPoints = 10;
 
 
+    // constructor
+    public GrowthDatabase(String filepath){
+        saveFilePath = filepath;
+
+        load(saveFilePath);
+    }
+
+    // load from save file
     public void load(String filepath){
 
     }
 
+    // save to file
     public void save(String filepath){
 
     }
 
+    // returns safeFilePath
     public String getSaveFilePath(){
         return saveFilePath;
+    }
+
+    // returns current state of forest growth
+    public int getCurrentForestGrowth(){
+        int result = activityPoints / 2;
+
+        return result;
     }
 }
